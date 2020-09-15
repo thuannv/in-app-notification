0e90652757c1c394f3c0a5b14ab3e46528e1dfb4
thuannv <nguyenvanthuan.vnit@gmail.com>
2020-09-15 11:24:05 +0700

    Refactored demo

--- app/src/main/java/com/github/thuannv/inappnotification/sample/MainActivity.kt
+++ app/src/main/java/com/github/thuannv/inappnotification/sample/MainActivity.kt
@@ -5,31 +5,44 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+object Controller {
 
-    private var depth: Int = 0
+    private const val MAX_DEPTH = 5
+
+    var depth: Int = 1
+
+    var direction: Int = 1
+
+    fun op() {
+        depth += direction
+        if (depth >= MAX_DEPTH) {
+            direction = -1
+        } else if (depth == 1) {
+            direction = 1
+        }
+    }
+}
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        depth = intent.getIntExtra(DEPTH, 0)
-
-        title = if (depth > 0) "Demo depth$depth" else "Demo"
+        title = "Demo activity count = ${Controller.depth}"
 
         button_toggle_view.setOnClickListener {
-            if (depth < MAX_DEPTH) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra(DEPTH, depth + 1)
-                startActivity(intent)
+            if (Controller.direction > 0) {
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
                 finish()
             }
+            Controller.op()
         }
     }
 
-    companion object {
-        const val DEPTH = "depth"
-        const val MAX_DEPTH = 5
+    override fun onResume() {
+        super.onResume()
+        button_toggle_view.text = if (Controller.direction > 0) "New Activity" else "Kill Activity"
     }
 }
